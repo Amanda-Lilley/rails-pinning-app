fb3fc48b93bae41b9dc76c51ac0d17f143e42f03
Amanda Lilley <yoknitting24@gmail.com>
2016-02-16 12:27:38 -0500

    set config environments

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -23,17 +23,32 @@ class PinsController < ApplicationController
 
     if @pin.valid?
       @pin.save
-    redirect_to pin_path(@pin)
-  else
-    @errors = @pin.errors
-    render :new
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :new
+    end
   end
+
+  def edit
+    @pin = Pin.find(params[:id])
+  render :edit
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+    if @pin.update(pin_params)
+      redirect_to pin_path(@pin)
+    else
+      @errors = @pin.errors
+      render :edit
+    end
   end
 
   private
 
   def pin_params
-    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id, :image)
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,16 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  Paperclip.options[:command_path] = "/usr/local/bin/convert"
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => "skillcrush-rails",
+    :access_key_id => "AKIAI4Y7CIUPPOFAYN2Q",
+    :secret_access_key => "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"
+  }
+}
 end
