203a5334cfa60cf2ec12d232319f59416841ed5a
Amanda Lilley <yoknitting24@gmail.com>
2016-02-16 21:25:30 -0500

    attempts to add authentication

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email, :password
+  validates_uniqueness_of :email
+  has_secure_password
+
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+
+    if !@user.nil?
+       @user.authenticate(password)
+        return @user
+      else
+        return nil
+    end
+  end
+
+end
